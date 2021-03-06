4030d68367b096916bd57e3ef1c59fdb1d8f543b
Nong Van Tinh <vannongtinh@gmail.com>
2021-03-06 20:36:59 +0700

    - Seprate definition of function to avoid error multiple definition of funtion.

---
+++ facilities.cpp
@@ -0,0 +1,18 @@
+#include "facilities.h"
+
+#include <iostream>
+
+void algorithms::print(const std::vector<double>& v)
+{
+	std::cout << "{ ";
+	for (size_t i(0); i < v.size(); ++i)
+	{
+		std::cout << v[i];
+		if (i != v.size() - 1)
+		{
+			std::cout << ", ";
+		}
+	}
+
+	std::cout << " }\n";
+}
--- facilities.h
+++ facilities.h
@@ -1,14 +1,6 @@
-void print(const std::vector<double>& v)
-{
-	std::cout << "{ ";
-	for (size_t i(0); i < v.size(); ++i)
-	{
-		std::cout << v[i];
-		if (i != v.size() - 1)
-		{
-			std::cout << ", ";
-		}
-	}
+#include <vector>
 
-	std::cout << " }\n";
+namespace algorithms
+{
+	void print(const std::vector<double>& v);
 }
