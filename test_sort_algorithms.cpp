af38b98482a39fbea6c6520eb4465a1433479262
Nong Van Tinh <vannongtinh@gmail.com>
2021-03-11 05:17:11 +0700

    - Update test.

--- test_sort_algorithms.cpp
+++ test_sort_algorithms.cpp
@@ -6,102 +6,124 @@
 
 bool algorithms::test::sort::test_insertion_sort()
 {
-	std::vector<double> v(10000);
-	for (size_t i = 0; i < v.size(); ++i)
+	std::vector<double> v;
+	for(size_t i(0); i < 100; ++i)	
 	{
-		v[i] = rand() % (50000 - -50000 + 1) + -50000;
-	}
 
-	char oper = '>';
-	algorithms::sort::insertion_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] > v[i - 1])
-			return false;
-	}
-	oper = '<';
-	algorithms::sort::insertion_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] < v[i - 1])
-			return false;
-	}
+		v.resize((int)(rand() % (50000 - 0 + 1) + 0));
+		for (size_t i = 0; i < v.size(); ++i)
+		{
+			v[i] = rand() % (50000 - -50000 + 1) + -50000;
+		}
 
-	return true;
+		char oper = '>';
+		algorithms::sort::insertion_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] > v[i - 1])
+				return false;
+		}
+		oper = '<';
+		algorithms::sort::insertion_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] < v[i - 1])
+				return false;
+		}
+
+		return true;
+	}
 }
 
 bool algorithms::test::sort::test_selection_sort()
 {
-	std::vector<double> v(10000);
-	for (size_t i = 0; i < v.size(); ++i)
+	std::vector<double> v;
+	for(size_t i(0); i < 100; ++i)	
 	{
-		v[i] = rand() % 50000 - 25000;
+
+		v.resize((int)(rand() % (50000 - 0 + 1) + 0));
+		for (size_t i = 0; i < v.size(); ++i)
+		{
+			v[i] = rand() % (50000 - -50000 + 1) + -50000;
+		}
+
+		char oper = '>';
+		algorithms::sort::selection_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] > v[i - 1])
+				return false;
+		}
+		oper = '<';
+		algorithms::sort::selection_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] < v[i - 1])
+				return false;
+		}
+
+		return true;
 	}
-	char oper = '>';
-	algorithms::sort::selection_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] > v[i - 1])
-			return false;
-	}
-	oper = '<';
-	algorithms::sort::selection_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] < v[i - 1])
-			return false;
-	}
-	return true;
 }
 
 bool algorithms::test::sort::test_bubble_sort()
 {
-	std::vector<double> v(10000);
-	for (size_t i = 0; i < v.size(); ++i)
+	std::vector<double> v;
+	for(size_t i(0); i < 100; ++i)	
 	{
-		v[i] = rand() % (50000 - -50000 + 1) + -50000;
-	}
 
-	char oper = '>';
-	algorithms::sort::bubble_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] > v[i - 1])
-			return false;
-	}
-	oper = '<';
-	algorithms::sort::bubble_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] < v[i - 1])
-			return false;
-	}
+		v.resize((int)(rand() % (50000 - 0 + 1) + 0));
+		for (size_t i = 0; i < v.size(); ++i)
+		{
+			v[i] = rand() % (50000 - -50000 + 1) + -50000;
+		}
 
-	return true;
+		char oper = '>';
+		algorithms::sort::bubble_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] > v[i - 1])
+				return false;
+		}
+		oper = '<';
+		algorithms::sort::bubble_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] < v[i - 1])
+				return false;
+		}
+
+		return true;
+	}
 }
 
 bool algorithms::test::sort::test_merge_sort()
 {
-	std::vector<double> v(10000);
-	for (size_t i = 0; i < v.size(); ++i)
+	std::vector<double> v;
+	for(size_t i(0); i < 100; ++i)	
 	{
-		v[i] = rand() % (50000 - -50000 + 1) + -50000;
-	}
 
-	char oper = '>';
-	algorithms::sort::merge_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] > v[i - 1])
-			return false;
-	}
-	oper = '<';
-	algorithms::sort::merge_sort(v, 0, v.size(), oper);
-	for (size_t i = 1; i < v.size(); ++i)
-	{
-		if (v[i] < v[i - 1])
-			return false;
-	}
+		v.resize((int)(rand() % (50000 - 0 + 1) + 0));
+		for (size_t i = 0; i < v.size(); ++i)
+		{
+			v[i] = rand() % (50000 - -50000 + 1) + -50000;
+		}
 
-	return true;
+		char oper = '>';
+		algorithms::sort::merge_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] > v[i - 1])
+				return false;
+		}
+		oper = '<';
+		algorithms::sort::merge_sort(v, 0, v.size(), oper);
+		for (size_t i = 1; i < v.size(); ++i)
+		{
+			if (v[i] < v[i - 1])
+				return false;
+		}
+
+		return true;
+	}
 }
