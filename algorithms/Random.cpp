d0bbf60ef3da0094202a644207be4c5ed0127ffc
Nong Van Tinh <vannongtinh@gmail.com>
2021-05-30 12:14:48 +0700

    - Move definition of funcction random_bool out header file.

--- algorithms/Random.cpp
+++ algorithms/Random.cpp
@@ -1,1 +1,9 @@
 #include "Random.h"
+
+bool dsaa::random::random_bool(double p_probability_of_true)
+{
+    std::random_device device;
+    std::mt19937 engine(device());
+    std::bernoulli_distribution distribution(p_probability_of_true);
+    return distribution(engine);
+}
