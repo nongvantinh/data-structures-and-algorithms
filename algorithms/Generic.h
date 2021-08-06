497f6afde46c5fa36c94eee2e9e42724d395da85
Nong Van Tinh <vannongtinh@gmail.com>
2021-08-07 06:42:37 +0700

    - Organize file structure.

---
+++ algorithms/Generic.h
@@ -0,0 +1,95 @@
+#include <functional>
+
+#include "Random.h"
+
+namespace dsaa
+{
+	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
+	RIterator merge(RIterator p_first, RIterator p_mid, RIterator p_last, Compare p_compare = Compare());
+
+	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
+	RIterator partition(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
+
+	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
+	RIterator randomized_partition(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
+}
+
+
+template <typename RIterator, typename Compare>
+RIterator dsaa::merge(RIterator p_first, RIterator p_mid, RIterator p_last, Compare p_compare)
+{
+	if (p_first == p_last)
+		return p_first;
+	size_t arr1_size = p_mid - p_first;
+	size_t arr2_size = p_last - p_mid;
+	std::vector<typename std::iterator_traits<RIterator>::value_type> left(arr1_size);
+	std::vector<typename std::iterator_traits<RIterator>::value_type> right(arr2_size);
+
+	for (size_t i(0); i < arr1_size; ++i)
+		left[i] = *(p_first + i);
+
+	for (size_t i(0); i < arr2_size; ++i)
+		right[i] = *(p_mid + i);
+
+	size_t i(0), m(0);
+	for (RIterator k(p_first); k != p_last; ++k)
+	{
+		if (arr1_size <= i)
+		{
+			*k = right[m];
+			++m;
+		}
+		else if (arr2_size <= m)
+		{
+			*k = left[i];
+			++i;
+		}
+		else if (p_compare(left[i], right[m]))
+		{
+			*k = left[i];
+			++i;
+		}
+		else
+		{
+			*k = right[m];
+			++m;
+		}
+	}
+	return p_last;
+}
+
+template <typename RIterator, typename Compare>
+RIterator dsaa::partition(RIterator p_first, RIterator p_last, Compare p_compare)
+{
+	auto pivot = *(p_last - 1);
+	RIterator i = p_first;
+	bool is_first(true); // To adapt to iterator, in case bound checking of iterator is turned on.
+
+	for (RIterator k(p_first); (p_last - 1) != k; ++k)
+	{
+		if (p_compare(*k, pivot))
+		{
+			if (is_first)
+				is_first = false;
+			else
+				++i;
+			std::swap(*i, *k);
+		}
+	}
+	// Special case we never ever do swap.
+	if (is_first)
+		std::swap(*i, pivot);
+	else
+		std::swap(*++i, pivot);
+
+	return i;
+}
+
+template <typename RIterator, typename Compare>
+RIterator dsaa::randomized_partition(RIterator p_first, RIterator p_last, Compare p_compare)
+{
+	size_t i(0);
+	i = dsaa::random::random_range_int(0, static_cast<int>((p_last - p_first) - 1));
+	std::swap(*(p_first + i), *(p_last - 1));
+	return dsaa::partition(p_first, p_last, p_compare);
+}
--- algorithms/Sort.h
+++ algorithms/Sort.h
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <iterator>
 
-#include "Random.h"
+#include "Generic.h"
 #include "Heap.h"
 
 namespace dsaa
@@ -26,9 +26,6 @@ namespace dsaa
 	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
 	RIterator merge_sort(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
 
-	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
-	RIterator merge(RIterator p_first, RIterator p_mid, RIterator p_last, Compare p_compare = Compare());
-
 	template <typename RIterator, typename Compare = std::greater_equal<typename std::iterator_traits<RIterator>::value_type>>
 	RIterator sort_heap(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
 
@@ -36,14 +33,7 @@ namespace dsaa
 	RIterator quick_sort(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
 
 	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
-	RIterator partition(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
-
-	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
 	RIterator randomized_quick_sort(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
-
-	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
-	RIterator randomized_partition(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
-
 }
 
 template <typename IIterator, typename SortBy>
@@ -152,49 +142,6 @@ RIterator dsaa::merge_sort(RIterator p_first, RIterator p_last, Compare p_compar
 }
 
 template <typename RIterator, typename Compare>
-RIterator dsaa::merge(RIterator p_first, RIterator p_mid, RIterator p_last, Compare p_compare)
-{
-	if (p_first == p_last)
-		return p_first;
-	size_t arr1_size = p_mid - p_first;
-	size_t arr2_size = p_last - p_mid;
-	std::vector<typename std::iterator_traits<RIterator>::value_type> left(arr1_size);
-	std::vector<typename std::iterator_traits<RIterator>::value_type> right(arr2_size);
-
-	for (size_t i(0); i < arr1_size; ++i)
-		left[i] = *(p_first + i);
-
-	for (size_t i(0); i < arr2_size; ++i)
-		right[i] = *(p_mid + i);
-
-	size_t i(0), m(0);
-	for (RIterator k(p_first); k != p_last; ++k)
-	{
-		if (arr1_size <= i)
-		{
-			*k = right[m];
-			++m;
-		}
-		else if (arr2_size <= m)
-		{
-			*k = left[i];
-			++i;
-		}
-		else if (p_compare(left[i], right[m]))
-		{
-			*k = left[i];
-			++i;
-		}
-		else
-		{
-			*k = right[m];
-			++m;
-		}
-	}
-	return p_last;
-}
-
-template <typename RIterator, typename Compare>
 RIterator dsaa::sort_heap(RIterator p_first, RIterator p_last, Compare p_compare)
 {
 	if (p_first == p_last)
@@ -225,33 +172,6 @@ RIterator dsaa::quick_sort(RIterator p_first, RIterator p_last, Compare p_compar
 }
 
 template <typename RIterator, typename Compare>
-RIterator dsaa::partition(RIterator p_first, RIterator p_last, Compare p_compare)
-{
-	auto pivot = *(p_last - 1);
-	RIterator i = p_first;
-	bool is_first(true); // To adapt to iterator, in case bound checking of iterator is turned on.
-
-	for (RIterator k(p_first); (p_last - 1) != k; ++k)
-	{
-		if (p_compare(*k, pivot))
-		{
-			if (is_first)
-				is_first = false;
-			else
-				++i;
-			std::swap(*i, *k);
-		}
-	}
-	// Special case we never ever do swap.
-	if (is_first)
-		std::swap(*i, pivot);
-	else
-		std::swap(*++i, pivot);
-
-	return i;
-}
-
-template <typename RIterator, typename Compare>
 RIterator dsaa::randomized_quick_sort(RIterator p_first, RIterator p_last, Compare p_compare)
 {
 	if (p_first == p_last)
@@ -264,15 +184,6 @@ RIterator dsaa::randomized_quick_sort(RIterator p_first, RIterator p_last, Compa
 		dsaa::randomized_quick_sort(pivot_iter + 1, p_last, p_compare);
 	}
 	return p_last;
-}
-
-template <typename RIterator, typename Compare>
-RIterator dsaa::randomized_partition(RIterator p_first, RIterator p_last, Compare p_compare)
-{
-	size_t i(0);
-	i = dsaa::random::random_range_int(0, static_cast<int>((p_last - p_first) - 1));
-	std::swap(*(p_first + i), *(p_last - 1));
-	return dsaa::partition(p_first, p_last, p_compare);
 }
 
 #endif // !DSAA_SORT_H
