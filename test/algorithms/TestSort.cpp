f4a2d206e3640317827624b339575d1ceecb41e9
Nong Van Tinh <vannongtinh@gmail.com>
2021-09-01 09:42:00 +0700

    Add unit test for randomized_tail_hoare_quick_sort

--- test/algorithms/TestSort.cpp
+++ test/algorithms/TestSort.cpp
@@ -1684,6 +1684,134 @@ TEST_CASE("Test randomized_tail_lomuto_quick_sort.", "[Sort]")
     }
 }
 
+TEST_CASE("Test randomized_tail_hoare_quick_sort.", "[Sort]")
+{
+    SECTION("An ordinary sequence.")
+    {
+        size_t arr_size(4);
+
+        dsaa::DynamicArray<int> param(dsaa::random::random_range_ints<int>(arr_size));
+        dsaa::DynamicArray<TestObject<int>> arr(param.begin(), param.end());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("The empty sequence.")
+    {
+        dsaa::DynamicArray<TestObject<int>> arr;
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("Just one element.")
+    {
+        size_t arr_size(1);
+
+        dsaa::DynamicArray<int> param(dsaa::random::random_range_ints<int>(arr_size));
+        dsaa::DynamicArray<TestObject<int>> arr(param.begin(), param.end());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("Even number of elements.")
+    {
+        size_t arr_size(6);
+
+        dsaa::DynamicArray<int> param(dsaa::random::random_range_ints<int>(arr_size));
+        dsaa::DynamicArray<TestObject<int>> arr(param.begin(), param.end());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("Odd number of elements.")
+    {
+        size_t arr_size(7);
+
+        dsaa::DynamicArray<int> param(dsaa::random::random_range_ints<int>(arr_size));
+        dsaa::DynamicArray<TestObject<int>> arr(param.begin(), param.end());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("All elements equal.")
+    {
+        size_t arr_size(8);
+
+        TestObject<int> value(dsaa::random::random_range_int<int>());
+        dsaa::DynamicArray<TestObject<int>> arr(arr_size, value);
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("Difference element at begining.")
+    {
+        size_t arr_size(9);
+
+        TestObject<int> value(dsaa::random::random_range_int<int>());
+        dsaa::DynamicArray<TestObject<int>> arr(arr_size, value);
+
+        int64_t index(0);
+        while (arr[index] == arr[index + 1])
+            arr[index] = TestObject<int>(dsaa::random::random_range_int<int>());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("Difference element at end.")
+    {
+        size_t arr_size(10);
+
+        TestObject<int> value(dsaa::random::random_range_int<int>());
+        dsaa::DynamicArray<TestObject<int>> arr(arr_size, value);
+
+        int64_t index(arr.size() - 1);
+        while (arr[index] == arr[index - 1])
+            arr[index] = TestObject<int>(dsaa::random::random_range_int<int>());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("A very large sequence.")
+    {
+        size_t arr_size(679);
+
+        dsaa::DynamicArray<int> param(dsaa::random::random_range_ints<int>(arr_size));
+        dsaa::DynamicArray<TestObject<int>> arr(param.begin(), param.end());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+
+    SECTION("Some sequences with a random number of elements.")
+    {
+        size_t arr_size(51);
+
+        dsaa::DynamicArray<int> param(dsaa::random::random_range_ints<int>(arr_size));
+        dsaa::DynamicArray<TestObject<int>> arr(param.begin(), param.end());
+
+        dsaa::randomized_tail_hoare_quick_sort(arr.begin(), arr.end());
+
+        REQUIRE(dsaa::is_sorted(arr.begin(), arr.end(), std::less_equal<TestObject<int>>()));
+    }
+}
+
 #include "MinHeap.h"
 TEST_CASE("Test dsaa::heap_property for dsaa::MinHeap.", "[dsaa::heap_property]")
 {
