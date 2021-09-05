06b3bb695044ccdea5166ee8db79e57e08ce8eea
Nong Van Tinh <vannongtinh@gmail.com>
2021-09-05 22:47:16 +0700

    Add counting_sort

--- algorithms/Sort.h
+++ algorithms/Sort.h
@@ -5,6 +5,7 @@
 #include <functional>
 #include <algorithm>
 #include <iterator>
+#include <stdexcept>
 
 #include "Generic.h"
 #include "Heap.h"
@@ -52,6 +53,12 @@ namespace dsaa
 
 	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>>
 	RIterator randomized_tail_hoare_quick_sort(RIterator p_first, RIterator p_last, Compare p_compare = Compare());
+
+	// Produces a sorted array.
+	// IntType The type of element in array. The effect is undefined if this is not one of
+	// short, int, long, long long, unsigned short, unsigned int, unsigned long, or unsigned long long.
+	template <typename RIterator, typename Compare = std::less<typename std::iterator_traits<RIterator>::value_type>, typename IntType = typename std::iterator_traits<RIterator>::value_type>
+	RIterator counting_sort(RIterator p_first, RIterator p_last, Compare p_compare = Compare(), IntType p_min = std::numeric_limits<IntType>::min(), IntType p_max = std::numeric_limits<IntType>::max());
 }
 
 template <typename IIterator, typename SortBy>
@@ -294,4 +301,51 @@ RIterator dsaa::randomized_tail_hoare_quick_sort(RIterator p_first, RIterator p_
 	return p_last;
 }
 
+template <typename RIterator, typename Compare, typename IntType>
+RIterator dsaa::counting_sort(RIterator p_first, RIterator p_last, Compare, IntType p_min, IntType p_max)
+{
+	if (p_first == p_last)
+		return p_last;
+
+	if (std::numeric_limits<IntType>::min() == p_min)
+		p_min = *std::min_element(p_first, p_last);
+	if (std::numeric_limits<IntType>::max() == p_max)
+		p_max = *std::max_element(p_first, p_last);
+
+	dsaa::DynamicArray<IntType> count_table(p_max - p_min + 1);
+
+	// Start counting.
+	auto begin(p_first);
+	while (begin != p_last)
+		++count_table[*(begin++) - p_min];
+
+	// Detect offset for indexes.
+	for (auto iter(count_table.begin() + 1); iter != count_table.end(); ++iter)
+		*iter += *(iter - 1);
+
+	dsaa::DynamicArray<IntType> result(p_last - p_first);
+
+	for (auto iter(p_last - 1); p_first <= iter; --iter)
+	{
+		result[count_table[*iter - p_min] - 1] = *iter;
+
+		--count_table[*iter - p_min];
+	}
+
+	// Returns result.
+	if (std::is_same<Compare, std::less<IntType>>::value || std::is_same<Compare, std::less_equal<IntType>>::value)
+	{
+		begin = p_first;
+		for (auto iter(result.begin()); iter != result.end(); ++iter, ++begin)
+			*begin = *iter;
+	}
+	else
+	{
+		begin = p_last - 1;
+		for (auto iter(result.begin()); iter != result.end(); ++iter, --begin)
+			*begin = *iter;
+	}
+
+	return p_last;
+}
 #endif // !DSAA_SORT_H
